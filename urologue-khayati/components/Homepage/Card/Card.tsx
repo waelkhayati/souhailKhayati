a9f861db9f02c99daacc60629599b960d877fec3
waelkhayati <khayatiwael@gmail.com>
2023-05-19 16:32:29 +0100

    section 2

---
+++ urologue-khayati/components/Homepage/Card/Card.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import style from './card.module.css'
+import { Stack } from '@mui/material'
+
+type CardProps = {
+    image: string,
+    alt: string,
+    title: string,
+    description: string,
+    link: string,
+    linkRef: string
+}
+
+export default function Card(props: CardProps) {
+  return (
+    <>
+        <Stack className={style.container} >
+            <img src={props.image} alt={props.alt}/>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+            <a href={props.linkRef}>{props.link}</a>
+        </Stack>
+    </>
+  )
+}
--- urologue-khayati/components/Homepage/Homepage.tsx
+++ urologue-khayati/components/Homepage/Homepage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import style from './homepage.module.css'
-import { Stack } from '@mui/material'
+import { Stack, Box } from '@mui/material'
+import Card from './Card/Card'
+import { I18nContext } from '../../pages/_app'
 
 export default function Homepage() {
+
+  const i18n = React.useContext(I18nContext);
+
   return (
     <>
       <Stack>
@@ -10,13 +15,14 @@ export default function Homepage() {
         <Stack className={style.section1}>
           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste facere quae animi esse, voluptas perspiciatis illum commodi mollitia possimus, dolorem odio quidem eum neque sed et! Magnam facere repellendus cum? Non magni reprehenderit maxime beatae aspernatur nam, porro officia, commodi dolores ab saepe qui iusto inventore eos quisquam hic necessitatibus.</p>
-          <a href="">Lorem ipsum dolor sit amet.</a>
+          <a href="">{i18n.more_about_out_practice}</a>
         </Stack>
-        <Stack className={style.section2}>
-          <Stack className={style.card}></Stack>
-          <Stack className={style.card}></Stack>
-          <Stack className={style.card}></Stack>
-        </Stack>
+        <Box className={style.section2}>
+          <Card image={'/assets/urology.jpg'} alt={''} title={i18n.urology} description={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, atque. Voluptas fugiat quisquam nesciunt provident.'} link={i18n.more_about_urology} linkRef={''}/>
+          <Card image={'/assets/andrology.jpg'} alt={''} title={i18n.andrology} description={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, atque. Voluptas fugiat quisquam nesciunt provident.'} link={i18n.more_about_andrology} linkRef={''}/>
+          <Card image={'/assets/radio_oncology.jpg'} alt={''} title={i18n.radiation_oncology } description={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, atque. Voluptas fugiat quisquam nesciunt provident.'} link={i18n.more_about_radiation_oncology} linkRef={''}/>
+          <Card image={'/assets/radio_therapy.jpg'} alt={''} title={i18n.radiation_therapy} description={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, atque. Voluptas fugiat quisquam nesciunt provident.'} link={i18n.more_about_radiation_therapy} linkRef={''}/>
+        </Box>
       </Stack>
     </>
   )
