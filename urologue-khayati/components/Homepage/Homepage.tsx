f64eda1a0501b25bde43eef56d47f5a5afddab34
waelkhayati <khayatiwael@gmail.com>
2023-05-19 15:00:54 +0100

    changed hero

--- urologue-khayati/components/Homepage/Homepage.tsx
+++ urologue-khayati/components/Homepage/Homepage.tsx
@@ -6,7 +6,7 @@ export default function Homepage() {
   return (
     <>
       <Stack>
-        <img src="/assets/hero.jpg" alt="" />
+        <img src="/assets/hero.jpg" alt="man_and_woman_lying_on_pillows_while_looking_at_each_other" />
         <Stack className={style.section1}>
           <h1>Ihr Frauenarzt in Hausmannstätten bei Graz</h1>
           <p>Zentral gelegen am Marktplatz von Hausmannstätten in der Nähe von Kalsdorf und Fernitz, steht Ihnen unsere Praxis in allen Fragen bzgl. der weiblichen Gesundheit zur Verfügung. Wir freuen uns auf Ihren Besuch in unserer Wahlarztpraxis!</p>
