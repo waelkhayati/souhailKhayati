860fc4d7d70251201cea802af7b3441b5e65035d
waelkhayati <khayatiwael@gmail.com>
2023-08-13 18:17:07 +0100

    meta

--- urologue-khayati/pages/index.tsx
+++ urologue-khayati/pages/index.tsx
@@ -10,6 +10,7 @@ import Navbar from '../components/Navbar/Navbar';
 import Practice from '../components/Practice/Practice';
 import Services from '../components/Services/Services';
 import style from '../styles/page.module.css';
+import Head from 'next/head';
 
 export default function Home() {
 
@@ -55,6 +56,95 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Urologe Khayati</title>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="Herzlich willkommen in der Wahlarztordination Dr. Souhail Khayati. Kontaktieren Sie uns noch heute, um einen Termin zu vereinbaren." />
+        <meta name="keywords" content="
+                                      Urologe Khayati, 
+                                      Urologie, 
+                                      Radiooncologie, 
+                                      Urooncologie, 
+                                      Urologie Hausmannstätten, 
+                                      Urologe Hausmannstätten, 
+                                      Andrologie, 
+                                      Souhail Khayati, 
+                                      Graz, 
+                                      Urologe Graz, 
+                                      Urologe Graz-Umgebung, 
+                                      Hausmannstätten, 
+                                      Österreichische Gesellschaft für Urologie und Andrologie, 
+                                      Eruopean Assosication of Urology, 
+                                      Arbeitskreis für Andrologie und sexuelle Funktionsstörungen, 
+                                      Österreichische Gesellschaft für Radioonkologie, 
+                                      Urologie Praxis Graz,
+                                      Facharzt für Urologie,
+                                      Urologische Untersuchungen,
+                                      Prostatakrebs Behandlung,
+                                      Männergesundheit,
+                                      Harnwegsinfektionen,
+                                      Urologische Chirurgie,
+                                      Blasenprobleme,
+                                      Hodenerkrankungen,
+                                      Nierensteinentfernung,
+                                      Urologische Diagnostik,
+                                      Andrologische Beratung,
+                                      Prostatauntersuchung,
+                                      Urologische Vorsorge,
+                                      Urologische Rehabilitation,
+                                      Urologische Therapieoptionen,
+                                      Inkontinenzbehandlung,
+                                      Urologische Praxis Hausmannstätten,
+                                      Dr. Souhail Khayati,
+                                      Urologe Graz-Umgebung,
+                                      Urologische Gesundheitsberatung,
+                                      Urologische Untersuchungsmethoden,
+                                      Urologische Krankheiten,
+                                      Urologische Operationen"
+                                      />
+        <meta name="Souhail Khayati" content="Urologe Khayati"/>
+        
+        <link rel="icon" href="assets/favicon.ico" />
+
+        <meta property="og:title" content="Urologe-Khayati"/>
+        <meta property="og:description" content="Herzlich willkommen in der Wahlarztordination Dr. Souhail Khayati. Kontaktieren Sie uns noch heute, um einen Termin zu vereinbaren."/>
+        <meta property="og:image" content="/assets/hero.jpg"/>
+        <meta property="og:url" content="https://www.urologe-khayati.at"/>
+
+        <meta property="twitter:title" content="Urologe-Khayati"/>
+        <meta property="twitter:description" content="Herzlich willkommen in der Wahlarztordination Dr. Souhail Khayati. Kontaktieren Sie uns noch heute, um einen Termin zu vereinbaren."/>
+        <meta property="twitter:image" content="/assets/hero.jpg"/>
+        <meta property="twitter:url" content="https://www.urologe-khayati.at"/>
+
+        <link rel="canonical" href="https://www.urologe-khayati.at"/>
+
+        <script type="application/ld+json">
+          {JSON.stringify({
+              "@context": "http://schema.org",
+              "@type": "LocalBusiness",
+              "name": "Urologe Khayati",
+              "description": "Herzlich willkommen in der Wahlarztordination Dr. Souhail Khayati. Kontaktieren Sie uns noch heute, um einen Termin zu vereinbaren.",
+              "url": "https://www.urologe-khayati.at",
+              "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Marktpl. 4/1",
+                  "addressLocality": "Hausmannstätten",
+                  "postalCode": "8071",
+                  "addressCountry": "Austria"
+              },
+              "contactPoint": {
+                  "@type": "ContactPoint",
+                  "telephone": "+43 664 1441636",
+                  "contactType": "Ordination"
+              },
+              "openingHours": "Freitag und Mittwoch von 16h bis 20h"
+          })}
+      </script>
+
+
+
+      </Head>
       <Stack className={style.background}>
         <Stack className={style.body}>
           <Header />
